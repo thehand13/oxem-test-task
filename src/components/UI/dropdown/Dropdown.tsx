2837ae61fffd9bc8d2196a195837233952d78c24
Roman <thehand@yandex.ru>
2023-02-22 13:27:54 +0300

    Fixed dropdown menu

--- src/components/UI/dropdown/Dropdown.tsx
+++ src/components/UI/dropdown/Dropdown.tsx
@@ -7,39 +7,49 @@ const Dropdown: React.FC<{ navLink: INavLink }> = ({ navLink }) => {
   const dropDownRef = useRef<HTMLDivElement>(null);
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const handleLinkClick = (e: any) => {
+  const linkClickHandler = (event: React.MouseEvent<HTMLElement>) => {
+    window.location.href = navLink.route;
+  };
+
+  const linkMouseOverHandler = (event: React.MouseEvent<HTMLElement>) => {
     if (navLink.subItems && navLink.subItems.length > 0) {
-      e.preventDefault();
+      event.preventDefault();
       setShowDropdown(true);
-    } else {
-      window.location.href = navLink.route;
+    }
+  };
+
+  const linkMouseLeaveHandler = (event: React.MouseEvent<HTMLElement>) => {
+    if (!(navLink.subItems && navLink.subItems.length > 0)) {
+      setShowDropdown(false);
     }
   };
 
   useEffect(() => {
-    const handleClickOutside = ({ target }: MouseEvent) => {
+    const clickOutsideHandler = ({ target }: MouseEvent) => {
       // @ts-ignore
       if (dropDownRef.current && !dropDownRef.current.contains(target)) {
         setShowDropdown(false);
       }
     };
 
-    document.addEventListener('click', handleClickOutside, true);
+    document.addEventListener('click', clickOutsideHandler, true);
     return () => {
-      document.removeEventListener('click', handleClickOutside, true);
+      document.removeEventListener('click', clickOutsideHandler, true);
     };
   }, []);
 
   return (
     <>
       <li
-        className={`${styles.navItem} ${showDropdown && styles.navItemClicked}`}
+        onMouseEnter={linkMouseOverHandler}
+        onMouseLeave={linkMouseLeaveHandler}
+        className={`${styles.navItem} ${showDropdown && styles.navItemActive}`}
         key={navLink.route}
       >
         <Link
           className={styles.navItemLink}
           href={navLink.route}
-          onClick={handleLinkClick}
+          onClick={linkClickHandler}
         >
           {navLink.title}
         </Link>
@@ -48,7 +58,7 @@ const Dropdown: React.FC<{ navLink: INavLink }> = ({ navLink }) => {
         <div className={styles.dropdown} ref={dropDownRef}>
           {navLink.subItems?.map((subItem) => (
             <Link
-              key={navLink.route}
+              key={subItem.route}
               className={styles.dropdownLink}
               href={subItem.route}
             >
